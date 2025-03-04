d01ef18f5c58cac3829cd8536f01f39fb7d243cc
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-04 11:15:48 +0530

    Add files via upload

---
+++ Operations.py
@@ -0,0 +1,19 @@
+a = int(100)
+b = int(30)
+
+print("Addition: ", a+b)
+print("Subraction: ", a-b)
+print("Division: ", a/b)
+print("Floor Division: ", a//b)
+print("Exponent: ", a**b)
+print("Sqr Root: ", a**0.5)
+
+# comparison
+
+print("?EqualTo ", a==b)
+print("?NotEqualTo ", a!=b)
+print("?GreaterThan ", a>b)
+print("?LessThan ", a<b)
+
+c = a**b/3+a*b//9              # Expression
+print("Result: ", c)
