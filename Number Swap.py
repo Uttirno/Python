616bc85746f4875608e6e30be14e44c706709400
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-04 11:26:19 +0530

    Add files via upload

---
+++ Number Swap.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = int(input())
+
+print("Before interchanging, x: ", x)
+print("Before interchanging, y: ", y)
+
+z = y
+w = x
+x = z
+y = w
+
+print("After interchanging, x: ", x)
+print("After interchanging, y: ", y)
