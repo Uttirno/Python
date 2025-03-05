610a3657e489c27447917ca53df3f29cb213598b
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-05 17:39:25 +0530

    Add files via upload

---
+++ Exam Requirements.py
@@ -0,0 +1,7 @@
+i = int(input("What is your attendance(Out of 300 days):"))
+j = int(input("What is yoyr total class test marks? (Out of 100):"))
+
+if i> 200 and j>50:
+    print("Eligible for the exam")
+else:
+    print("uneligible for the exam")
