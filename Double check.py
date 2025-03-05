b1b7bc37b9be9a449b89257992ccbcf984fdbfaf
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-05 17:25:05 +0530

    Add files via upload

---
+++ Double check.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number"))
+
+if n > 50:
+    if n % 2 == 0:
+        print("the number is even")
+    else:
+        print("the number is odd")
+else:
+    print("The number is less than 50")
