7a4aae44ab85526c02a5b42efdf182267ec16ce7
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-14 13:08:57 +0530

    Add files via upload

---
+++ OperationOnDictionaries.py
@@ -0,0 +1,26 @@
+my_dict = {}
+
+# Make dict with integer keys:
+my_dict = {1:"apple", 2:"banana"}
+
+# Make a dict with mixed keys:
+
+my_dict = {"Name": "Uttirno", "Class": "Six", 1:"apple", 2:"banana"}
+
+# Output Keys:
+print(my_dict["Name"])
+print(my_dict["Class"])
+
+# update values
+my_dict["Class"] = "Seven"
+
+# Add item
+my_dict["Address"] = "Kolkata"
+
+# Access A particular element
+
+print("Address: ", "Kolkata")
+
+# Remove all elements:
+my_dict.clear()
+print(my_dict)
