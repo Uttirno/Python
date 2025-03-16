c8d593fbbb641bafd231ae6e34ea9ce54e42ba5a
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-16 19:24:45 +0530

    Add files via upload

---
+++ TupleOperations.py
@@ -0,0 +1,29 @@
+my_tuple = ()
+print(my_tuple)
+
+# tuple with ints
+my_tuple = (1, 2, 3, 4)
+
+print(my_tuple)
+
+my_tuple = ("mouse", ("Hello"), 1, 2, ["a", "b"], {"Key": "Value"}, {1, 2, 3})
+
+print("Tuple with mixed datatypes: ", my_tuple)
+
+my_tuple = (("Python", "Codingal"))
+
+print("nested tuple: ", my_tuple)
+
+my_tuple = ('a','b','c','d','e')
+
+print(my_tuple[0])
+print(my_tuple[4])
+
+my_tuple = (1, 2, 3, 4)
+
+print("sliced:",my_tuple[0:3])
+
+my_tuple = (1,2,3,4,5,6,7,8,9,0)
+
+for item in my_tuple:
+    print("Iteration: ", item)
