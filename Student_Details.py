4f84a7d19848e5e3ca39adc5def047c22a8591b7
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-19 11:09:11 +0530

    Add files via upload

---
+++ Student_Details.py
@@ -0,0 +1,16 @@
+class Person(object):
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.year = year
+
+z = Student("Joey", "King", 2021)
+z.printname()
+print(z.year)
