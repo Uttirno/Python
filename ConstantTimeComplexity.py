2101904ebb991adbc1e4b29d02c5efcc56e84d28
Uttirno <lahirisrimoyee993@gmail.com>
2025-03-25 10:58:47 +0530

    Add files via upload

---
+++ ConstantTimeComplexity.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iterations = 0
+    print("The number entered is: ", n)
+    iterations+= 1
+    print("Total iterations: ", iterations)
+
+printnumber(10)
+printnumber(300)
